affa7791181651db3eeeca3f57319bd1f7533f2e
Eng Christian <xtianm4@gmail.com>
2022-06-21 11:43:17 +0300

    deleted more

--- src/hooks/useAuth.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react"
-import { AuthContext } from "../context/AllContext"
-
-const useAuth = () => {
-    const authValues = useContext(AuthContext);
-    return authValues.auth
-}
-export default useAuth;
